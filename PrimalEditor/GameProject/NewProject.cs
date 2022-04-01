8a90ba29856b88382902b0f42c6827bb349621ab
Asus <Asus@TAKONOLAP>
2022-04-01 09:11:26 +0800

    Add project files.

---
+++ PrimalEditor/GameProject/NewProject.cs
@@ -0,0 +1,203 @@
+﻿using PrimalEditor.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace PrimalEditor.GameProject
+{
+    [DataContract]
+    public class ProjectTemplate
+    {
+        [DataMember]
+        public string ProjectType { get; set; }
+        [DataMember]
+        public string ProjectFile { get; set; }
+        [DataMember]
+        public List<string> Folders { get; set; }
+
+        public Byte[] Icon { get; set; }
+        public Byte[] Screenshot { get; set; }
+
+        public string IconFilePath { get; set; }
+        public string ScreenshotFilePath { get; set; }
+        public string ProjectFilePath { get; set; }
+    }
+
+    class NewProject : ViewModelBase
+    {
+        // TODO: get the path from the installation location
+        private readonly string _templatePath = @"..\..\PrimalEditor\ProjectTemplates";
+        private string _projectName = "NewProject";
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PrimalProjects\" ;
+        public string ProjectName
+        {
+            get => _projectName;
+            set
+            {
+                if(_projectName != value)
+                {
+                    _projectName = value;
+                    ValidateProjectPath();
+                    OnPropertyChanged(nameof(ProjectName));
+                }
+            }
+        }
+        public string ProjectPath
+        {
+            get => _projectPath;
+            set
+            {
+                if(_projectPath != value)
+                {
+                    _projectPath = value;
+                    ValidateProjectPath();
+                    OnPropertyChanged(nameof(ProjectPath));
+                }
+            }
+        }
+
+        private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
+        public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates { get; }
+
+        private bool _isValid;
+        public bool IsValid
+        {
+            get => _isValid;
+            set
+            {
+                if (_isValid != value)
+                {
+                    _isValid = value;
+                    OnPropertyChanged(nameof(IsValid));
+                }
+            }
+        }
+        private string _errorMsg;
+        public string ErrorMsg
+        {
+            get => _errorMsg;
+            set
+            {
+                if(_errorMsg != value)
+                {
+                    _errorMsg = value;
+                    OnPropertyChanged(nameof(ErrorMsg));
+                }
+            }
+        }
+        private bool ValidateProjectPath()
+        {
+            var path = ProjectPath;
+            if (!path.EndsWith(@"\"))
+            {
+                path += @"\";
+            }
+            path += $@"{ProjectName}\";
+
+            IsValid = false;
+            if (string.IsNullOrEmpty(ProjectName.Trim()))
+            {
+                ErrorMsg = "Type in a project name !";
+            }
+            else if (ProjectName.IndexOfAny(Path.GetInvalidFileNameChars()) != -1)
+            {
+                ErrorMsg = "Invalid character(s) in project name !";
+            }
+            else if (string.IsNullOrEmpty(ProjectPath.Trim()))
+            {
+                ErrorMsg = "Select a valid project folder";
+            }
+            else if (ProjectPath.IndexOfAny(Path.GetInvalidPathChars()) != -1)
+            {
+                ErrorMsg = "Invalid character(s) in project path !";
+            }
+            else if (Directory.Exists(path) && Directory.EnumerateFileSystemEntries(path).Any())
+            {
+                ErrorMsg = "Selected folder already exists and is not empty !";
+            }
+            else
+            {
+                ErrorMsg = string.Empty;
+                IsValid = true;
+            }
+            return IsValid; 
+        }
+
+        public string CreateProject(ProjectTemplate template)
+        {
+            ValidateProjectPath();
+            if (!IsValid)
+            {
+                return string.Empty;
+            }
+            var path = ProjectPath;
+            if (!path.EndsWith(@"\"))
+            {
+                path += @"\";
+            }
+            path += $@"{ProjectName}\";
+
+            try
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                    foreach(var folder in template.Folders)
+                    {
+                        Directory.CreateDirectory(Path.GetFullPath(Path.Combine(Path.GetDirectoryName(path), folder)));
+                    }
+                }
+                var dirInfo = new DirectoryInfo(path + @".primal\");
+                dirInfo.Attributes |= FileAttributes.Hidden;
+                File.Copy(template.IconFilePath, Path.GetFullPath(Path.Combine(dirInfo.FullName, "icon.png")));
+                File.Copy(template.ScreenshotFilePath, Path.GetFullPath(Path.Combine(dirInfo.FullName, "screenshot.png")));
+
+                //var project = new Project(ProjectName, path);
+                //Serializer.ToFile(project, path + $"{ProjectName}" + Project.Extention);
+                var projectXml = File.ReadAllText(template.ProjectFilePath);
+                projectXml = String.Format(projectXml, ProjectName, ProjectPath);
+                var projectPath = Path.GetFullPath(Path.Combine(path, $"{ProjectName}{Project.Extention}"));
+                File.WriteAllText(projectPath, projectXml);
+                return path;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return string.Empty;
+            }
+        }
+
+        public NewProject()
+        {
+            ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplates);
+            try
+            {
+                var templatesFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templatesFiles.Length > 0);
+                foreach(var file in templatesFiles)
+                {
+                    var template =  Serializer.FromFile<ProjectTemplate>(file);
+                    template.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file),"icon.png"));
+                    template.Icon = File.ReadAllBytes(template.IconFilePath);
+                    template.ScreenshotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file),"screenshot.png"));
+                    template.Screenshot = File.ReadAllBytes(template.ScreenshotFilePath);
+                    template.ProjectFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file),template.ProjectFile));
+                    _projectTemplates.Add(template);
+                }
+                ValidateProjectPath();
+            }
+            catch (Exception ex)
+            {   
+                Debug.WriteLine(ex.Message);
+                // TODO: log errors
+            }
+        }
+    }
+}
---
+++ PrimalEditor/GameProject/Project.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimalEditor.GameProject
+{
+    [DataContract(Name = "Game")]
+    public class Project : ViewModelBase
+    {
+        public static string Extention { get; } = ".primal";
+        [DataMember]
+        public string Name { get; private set; }
+        [DataMember]
+        public string Path { get; private set; }
+        public string FullPath => $"{Path}{Name}{Extention}";
+        [DataMember(Name = "Scenes")]
+        private ObservableCollection<Scene> _scenes = new ObservableCollection<Scene>();
+        public ReadOnlyCollection<Scene> Scenes
+        {
+            get;
+        }
+
+        public Project (string name, string path)
+        {
+            Name = name;
+            Path = path;
+
+            _scenes.Add(new Scene(this, "Default Scene"));
+        }
+    }
+}
