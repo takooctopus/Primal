54e625bf95e6ce4afdcc8bb474786c505601fbba
takooctopus <takooctopus@hotmail.com>
2022-04-02 14:04:13 +0800

    Loggers 1. 在LoggerView视图中实现了LogMessaage的分类呈现，简单的就直接使用filter来实现【仍旧没有开始实现持久化】

--- PrimalEditor/Utilities/LoggerView.xaml.cs
+++ PrimalEditor/Utilities/LoggerView.xaml.cs
@@ -23,6 +23,27 @@ namespace PrimalEditor.Utilities
         public LoggerView()
         {
             InitializeComponent();
+            // TODEL: Test
+            Loaded += (s, e) =>
+            {
+                Logger.Log(MessageType.Info, $"Log Info Test");
+                Logger.Log(MessageType.Warning, $"Log Warning Test");
+                Logger.Log(MessageType.Error, $"Log Error Test");
+            };
+        }
+
+        private void OnClear_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Logger.Clear();
+        }
+
+        private void OnMessageFilter_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var filter = 0x0;
+            if (toggleInfos.IsChecked == true) filter |= (int)MessageType.Info;
+            if (toggleWarnings.IsChecked == true) filter |= (int)MessageType.Warning;
+            if (toggleErrors.IsChecked == true) filter |= (int)MessageType.Error;
+            Logger.SetMessageFilter(filter);
         }
     }
 }
