5285a836eee606c241c960a9bd9b3bc13cb288e4
takooctopus <takooctopus@hotmail.com>
2022-04-04 14:06:21 +0800

    数字文本框控件 1. 实现基础的数字文本框控件，可以实现输入，拖动等改变控件的数字值 2. 基于NumberBox的标量显示盒，因为NumberBox只是一层基础的功能封装，定义了点击和拖动的特性，继承于NumberBox的ScalarBox在此基础上实现了一些样式的改变，比如鼠标滑动和不可用的事件样式[代码很多和下面VectorBox复用了] 3. 基于NumberBox的向量显示盒，主要是使用grid中封装几个NumberBox，使用Trigger来限定显示方式，实现组件的捆绑，以实现一组向量的同步绑定 4. !!! 等着DEBUG:  Error XDG0062 VisualTree 中未找到带有 Name“nbX”的子级。【我还不知道怎么回事】

--- PrimalEditor/Dictionaries/ControlTemplates.xaml.cs
+++ PrimalEditor/Dictionaries/ControlTemplates.xaml.cs
@@ -11,6 +11,8 @@ namespace PrimalEditor.Dictionaries
 {
     public partial class ControlTemplates : ResourceDictionary
     {
+        // TextBox *********************************************************************************************************************************
+
         private void OnTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
             var textBox = sender as TextBox;
@@ -36,6 +38,47 @@ namespace PrimalEditor.Dictionaries
             }
         }
 
+        private void OnTextBoxRename_KeyDown(object sender, KeyEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var exp = textBox.GetBindingExpression(TextBox.TextProperty);
+            if (exp == null) return;
+            if (e.Key == Key.Enter)
+            {
+                if (textBox.Tag is ICommand command && command.CanExecute(textBox.Text))
+                {
+                    command.Execute(textBox.Text);
+                }
+                else
+                {
+                    exp.UpdateSource();
+                }
+                textBox.Visibility = Visibility.Collapsed;
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Escape)
+            {
+                exp.UpdateTarget();
+                textBox.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void OnTextBoxRename_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var exp = textBox.GetBindingExpression(TextBox.TextProperty);
+            // 一旦失去焦点，我们就将值设置成原来的值，并将焦点目标设置成之前的焦点目标控件
+            if(exp != null)
+            {
+                exp.UpdateTarget();
+                textBox.MoveFocus(new TraversalRequest(FocusNavigationDirection.Previous));
+                textBox.Visibility = Visibility.Collapsed;
+            }
+        }
+
+
+        // Button *********************************************************************************************************************************
+
         private void OnClose_Button_Click(object sender, RoutedEventArgs e)
         {
             var window = ((sender as FrameworkElement).TemplatedParent) as Window;
@@ -55,5 +98,6 @@ namespace PrimalEditor.Dictionaries
             var window = ((sender as FrameworkElement).TemplatedParent) as Window;
             window.WindowState = WindowState.Minimized;
         }
+
     }
 }
---
+++ PrimalEditor/Utilities/Controls/NumberBox.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace PrimalEditor.Utilities.Controls
+{
+    [TemplatePart(Name = "PART_textBlock", Type=typeof(TextBlock))]
+    [TemplatePart(Name = "PART_textBox", Type=typeof(TextBox))]
+    internal class NumberBox : Control
+    {
+        // 用来保存鼠标按下时的原始值
+        private double _originalValue;
+        // 用来表示是否捕获到了鼠标
+        private bool _captured = false;
+        // 用来表示拖动时里面的数值到底改变没有
+        private bool _valueChanged = false;
+        // 按下去时应该记录鼠标的初始位置方便去计算移动距离
+        private double _mouseXStart;
+        // 倍率，设置同时按下ctrl或者shift来控制滑动的速率
+        private double _multiplier;
+        public string Value {
+            get => (string)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value); 
+        }
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(nameof(Value), typeof(string), typeof(NumberBox), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public double Multiplier
+        {
+            get => (double)GetValue(MultiplierProperty);
+            set => SetValue(MultiplierProperty, value);
+        }
+        public static readonly DependencyProperty MultiplierProperty = DependencyProperty.Register(nameof(Multiplier), typeof(double), typeof(NumberBox), new PropertyMetadata(1.0));
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            // 有textblock就绑定几个函数
+            if(GetTemplateChild("PART_textBlock") is TextBlock textBlock)
+            {
+                textBlock.MouseLeftButtonDown += OnTextBlock_Mouse_LBD;
+                textBlock.MouseLeftButtonUp += OnTextBlock_Mouse_LBU;
+                textBlock.MouseMove += OnTextBlock_Mouse_Move;
+            }
+        }
+
+        private void OnTextBlock_Mouse_LBD(object sender, MouseButtonEventArgs e)
+        {
+            double.TryParse(Value, out _originalValue);
+            Mouse.Capture(sender as UIElement);
+            _captured = true;
+            _valueChanged = false;
+            e.Handled = true;
+
+            // TODEBUG: 我TM就不明白这个GetPosition为什么返回来的X就是整数间隔的，就离谱。 明明Y那边都是好的，这边X就不是了!!!
+            _mouseXStart =  e.GetPosition(this).X;
+        }
+        private void OnTextBlock_Mouse_LBU(object sender, MouseButtonEventArgs e)
+        {
+            if (_captured)
+            {
+                Mouse.Capture(null);
+                _captured = false;
+                e.Handled = true;
+                // 要是是一个点击事件[即你按下去弹起来并没有拖动改变值，那么我们就显示下面的输入文本框，并将里面的值全部选中]
+                if (!_valueChanged && GetTemplateChild("PART_textBox") is TextBox textBox)
+                {
+                    textBox.Visibility = Visibility.Visible;
+                    textBox.Focus();
+                    textBox.SelectAll();
+                }
+            }
+        }
+        private void OnTextBlock_Mouse_Move(object sender, MouseEventArgs e)
+        {
+            if (_captured)
+            {
+                var mouseX = e.GetPosition(this).X;
+                var d = mouseX - _mouseXStart;
+                // 只要两者间距大于最小的系统拖拽间距
+                if(Math.Abs(d) > SystemParameters.MinimumHorizontalDragDistance)
+                {
+                    if (Keyboard.Modifiers.HasFlag(ModifierKeys.Control)) _multiplier = 0.001;
+                    else if (Keyboard.Modifiers.HasFlag(ModifierKeys.Shift)) _multiplier = 0.1;
+                    else _multiplier = 0.01;
+                    var newValue = _originalValue + (d * _multiplier * Multiplier);
+                    Value = newValue.ToString("0.#####");
+                    _valueChanged = true;
+                }
+            }
+        }
+
+        static NumberBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NumberBox), new FrameworkPropertyMetadata(typeof(NumberBox)));
+        }
+    }
+}
