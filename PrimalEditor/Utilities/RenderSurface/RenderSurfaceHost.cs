c3bc6b003e145d180a179d54c69a862e789cdcb7
takooctopus <takooctopus@hotmail.com>
2022-04-08 00:43:24 +0800

    内置win窗口到编辑器中 1. EngineDLL\EngineAPI.cpp又有俩暴露的方法，用来创建graphic 2. 相应的C#也要导入对应的API 3. Utilities新加入一个RenderSurfaceHost类用来保存创建的窗口handle引用，再添加一个UserControl[记得要继承IDisposable]类用于暴露_host[就是窗口的handle啦(这个继承于HwndHost)] 4. WorldEditor中引入这个UserControl

---
+++ PrimalEditor/Utilities/RenderSurface/RenderSurfaceHost.cs
@@ -0,0 +1,60 @@
+﻿using PrimalEditor.DllWrappers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+
+namespace PrimalEditor.Utilities
+{
+    /// <summary>
+    /// 窗口类,继承于【HwndHost】，用来生成窗口
+    /// </summary>
+    /// <seealso cref="System.Windows.Interop.HwndHost" />
+    internal class RenderSurfaceHost : HwndHost
+    {
+        private IntPtr _renderWindowHandle = IntPtr.Zero;
+        private readonly int _width = 800;
+        private readonly int _height = 600;
+
+        public int SurfaceId { get; private set; } = ID.INVALID_ID;
+
+        public RenderSurfaceHost(double width, double height)
+        {
+            _width = (int)width;
+            _height = (int)height;
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, creates the window to be hosted.
+        /// 调用EngineAPI创建窗口，返回handle引用入口
+        /// </summary>
+        /// <param name="hwndParent">The window handle of the parent window.</param>
+        /// <returns>
+        /// The handle to the child Win32 window to create.
+        /// </returns>
+        protected override HandleRef BuildWindowCore(HandleRef hwndParent)
+        {
+            SurfaceId = EngineAPI.CreateRenderSurface(hwndParent.Handle, _width, _height);
+            Debug.Assert(ID.IsValid(SurfaceId));
+            _renderWindowHandle = EngineAPI.GetWindowHandle(SurfaceId);
+            Debug.Assert(_renderWindowHandle != IntPtr.Zero);
+            return new HandleRef(this, _renderWindowHandle);
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, destroys the hosted window.
+        /// 退出时调用EngineApi删除界面
+        /// </summary>
+        /// <param name="hwnd">A structure that contains the window handle.</param>
+        protected override void DestroyWindowCore(HandleRef hwnd)
+        {
+            EngineAPI.RemoveRenderSurface(SurfaceId);
+            SurfaceId = ID.INVALID_ID;
+            _renderWindowHandle = IntPtr.Zero;
+        }
+    }
+}
