6172b35a62300cc0008b10bca066460334a61fc7
takooctopus <takooctopus@hotmail.com>
2022-04-10 12:28:43 +0800

    因为我们还没有自己的渲染器，使用WPF先看看 1. 创建一个窗口primitiveMeshDialog，用来创建mesh和预览，其中的函数主要就是一个UpdatePrimitive()，只要有参数变化，就调用API的PrimitiveMesh创建一个DataContext回来进行预览 2.  在GeometryEditor中我们要定义几个类，MeshRendererVertexData【定义所有顶点的属性，包含了镜面反射笔刷，漫反射笔刷，顶点位置集合，法线集合，UV集合和序号集合】 MeshRenderer【包含我们的MeshRendererVertexData集合， 相机的各种属性(比方说相机朝向，相机位置，相机对象，相机的偏移量，各种灯光的颜色) 以及一个初始化的函数】 GeometryEditor【设定其下属资源】 3. 然后是GeometryView相关，里面定义了鼠标对grid拖动等操作的反应，用来平移和缩放啥的

---
+++ PrimalEditor/Content/PrimitiveMeshDialog.xaml.cs
@@ -0,0 +1,71 @@
+﻿using PrimalEditor.ContentToolsAPIStruct;
+using PrimalEditor.DllWrappers;
+using PrimalEditor.Editors;
+using PrimalEditor.Utilities.Controls;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PrimalEditor.Content
+{
+    /// <summary>
+    /// Interaction logic for PrimitiveMeshDialog.xaml [建立几何图形的dialog]
+    /// </summary>
+    public partial class PrimitiveMeshDialog : Window
+    {
+
+        private void OnPrimitiveMeshType_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) => UpdatePrimitive();
+
+        private void OnSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) => UpdatePrimitive();
+
+        private void OnScalarBox_ValueChanged(object sender, RoutedEventArgs e) => UpdatePrimitive();
+
+        private float Value(ScalarBox scalarBox, float min)
+        {
+            float.TryParse(scalarBox.Value, out var result);
+            return Math.Max(result, min);
+        }
+        private void UpdatePrimitive()
+        {
+            if (!IsInitialized) return;
+            var primitiveType = (PrimitiveMeshType)PrimitiveMeshTypeComboBox.SelectedItem;
+            var info = new PrimitiveInitInfo()
+            {
+                Type = primitiveType,
+            };
+
+            switch (primitiveType)
+            {
+                case PrimitiveMeshType.Plane:
+                    {
+                        info.SegmentX = (int)xSliderPlane.Value;
+                        info.SegmentZ = (int)xSliderPlane.Value;
+                        info.Size.X = Value(widthScalarBoxPlane, 0.001f);
+                        info.Size.Z = Value(lengthScalarBoxPlane, 0.001f);
+                        break;
+                    }
+                case PrimitiveMeshType.Cube:
+                    break;
+                case PrimitiveMeshType.UvSphere:
+                    break;
+                case PrimitiveMeshType.IcoSphere:
+                    break;
+                case PrimitiveMeshType.Cyclinder:
+                    break;
+                case PrimitiveMeshType.Capsule:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(primitiveType));
+            }
+            var geometry = new Geometry();
+            ContentToolsAPI.CreatePrimitiveMesh(geometry, info);
+            (DataContext as GeometryEditor).SetAsset(geometry);
+        }
+
+        public PrimitiveMeshDialog()
+        {
+            InitializeComponent();
+            Loaded += (sender, args) => UpdatePrimitive();
+        }
+    }
+}
---
+++ PrimalEditor/Editors/GeometryEditor/GeometryView.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media.Media3D;
+
+namespace PrimalEditor.Editors
+{
+    /// <summary>
+    /// Interaction logic for GeometryView.xaml
+    /// </summary>
+    public partial class GeometryView : UserControl
+    {
+        private Point _clickedPosition;
+        private bool _captureLeft;
+        private bool _captureRight;
+
+        private void SetGeometry(int index = -1)
+        {
+            if (!(DataContext is MeshRenderer vm)) return;
+            if (vm.Meshes.Any() && viewport.Children.Count == 2)
+            {
+                // viewport第一个children是灯光，第二个是点集合，每次来新的，要把老的删了
+                viewport.Children.RemoveAt(1);
+            }
+            var meshIndex = 0;
+            var modelGroup = new Model3DGroup();
+            foreach (var mesh in vm.Meshes)
+            {
+                // 跳过我们不想要的mesh
+                if (index != -1 && meshIndex != index)
+                {
+                    ++meshIndex;
+                    continue;
+                }
+                var mesh3D = new MeshGeometry3D()
+                {
+                    Positions = mesh.Positions,
+                    Normals = mesh.Normals,
+                    TriangleIndices = mesh.Indices,
+                    TextureCoordinates = mesh.UVs
+                };
+                var diffuse = new DiffuseMaterial(mesh.Diffuse);
+                var specular = new SpecularMaterial(mesh.Specular, 50); //镜面反射
+                var matGroup = new MaterialGroup();
+                matGroup.Children.Add(diffuse);
+                matGroup.Children.Add(specular);
+
+                var model = new GeometryModel3D(mesh3D, matGroup);
+                modelGroup.Children.Add(model);
+
+                var binding = new Binding(nameof(mesh.Diffuse)) { Source = mesh };
+                BindingOperations.SetBinding(diffuse, DiffuseMaterial.BrushProperty, binding);
+
+                if (meshIndex == index) break;
+            }
+            var visual = new ModelVisual3D() { Content = modelGroup };
+            viewport.Children.Add(visual);
+        }
+        public GeometryView()
+        {
+            InitializeComponent();
+            DataContextChanged += (s, e) => SetGeometry();
+        }
+
+        private void OnGrid_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (!_captureLeft && !_captureRight) return;
+            var pos = e.GetPosition(this);
+            var d = pos - _clickedPosition;
+
+            if (_captureLeft && !_captureRight)
+            {
+                MoveCamera(d.X, d.Y, 0f);
+            }
+            else if (!_captureLeft && _captureRight)
+            {
+                var vm = DataContext as MeshRenderer;
+                var cp = vm.CameraPosition;
+                // 因为现在是xz平面，我们看y轴的偏移量，从上面看xz平面
+                var yOffset = d.Y * 0.001 * Math.Sqrt(cp.X * cp.X + cp.Z * cp.Z);
+                vm.CameraTarget = new Point3D(vm.CameraTarget.X, vm.CameraTarget.Y + yOffset, vm.CameraTarget.Z);
+            }
+
+            _clickedPosition = pos;
+        }
+
+        private void OnGrid_Mouse_LBD(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            _clickedPosition = e.GetPosition(this);
+            _captureLeft = true;
+            Mouse.Capture(sender as UIElement);
+        }
+
+        private void Grid_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            MoveCamera(0, 0, Math.Sign(e.Delta));
+        }
+
+        private void OnGrid_Mouse_LBU(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            _captureLeft = false;
+            if (!_captureRight) Mouse.Capture(null);
+        }
+
+        private void OnGrid_Mouse_RBD(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            _clickedPosition = e.GetPosition(this);
+            _captureRight = true;
+            Mouse.Capture(sender as UIElement);
+        }
+
+        private void OnGrid_Mouse_RBU(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            _captureRight = false;
+            if(!_captureLeft) Mouse.Capture(null);
+        }
+
+        /// <summary>
+        /// 平移镜头
+        /// </summary>
+        /// <param name="dx"></param>
+        /// <param name="dy"></param>
+        /// <param name="dz"></param>
+        private void MoveCamera(double dx, double dy, double dz)
+        {
+            var vm = DataContext as MeshRenderer;
+            var v = new Vector3D(vm.CameraPosition.X, vm.CameraPosition.Y, vm.CameraPosition.Z);
+
+            //  我们求在左手极坐标坐标系中的r,θ和φ角度【但是因为我们的标准坐标是右手坐标系的，这个会导致yz轴互换并使得z反号】
+            var r = v.Length;   // 计算现在相机位置的半径r
+            var theta = Math.Acos(v.Y / r); //θ角 竖向上的角
+            var phi = Math.Atan2(-v.Z, v.X);  //Φ角 xz平面上的角 ()
+
+            // 因为dxdy都很小
+            theta -= dy * 0.01;
+            phi -= dx * 0.01;
+            r *= 1.0 - 0.1 * dz;
+
+            theta = Math.Clamp(theta, 0.0001, Math.PI - 0.0001);
+
+            v.X = r * Math.Sin(theta) * Math.Cos(phi);
+            v.Z = -r * Math.Sin(theta) * Math.Sin(phi);
+            v.Y = r * Math.Cos(theta);
+
+            vm.CameraPosition = new Point3D(v.X, v.Y, v.Z);
+        }
+
+    }
+}
