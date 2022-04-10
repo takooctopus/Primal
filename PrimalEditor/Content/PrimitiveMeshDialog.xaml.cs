d5173580a6dd5e8ba97fe07cda17cdeb88dac488
takooctopus <takooctopus@hotmail.com>
2022-04-10 14:29:42 +0800

    将uv纹理放到平面上 1. uv显示的绑定错误fix 1. 通过uri创建图片BitmapSource，再创建笔刷imagebrush，再将笔刷绑定到mesh.diffuse上

--- PrimalEditor/Content/PrimitiveMeshDialog.xaml.cs
+++ PrimalEditor/Content/PrimitiveMeshDialog.xaml.cs
@@ -3,8 +3,12 @@ using PrimalEditor.DllWrappers;
 using PrimalEditor.Editors;
 using PrimalEditor.Utilities.Controls;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace PrimalEditor.Content
 {
@@ -13,6 +17,8 @@ namespace PrimalEditor.Content
     /// </summary>
     public partial class PrimitiveMeshDialog : Window
     {
+
+        private static readonly List<ImageBrush> _textures = new List<ImageBrush>();
 
         private void OnPrimitiveMeshType_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) => UpdatePrimitive();
 
@@ -39,33 +45,77 @@ namespace PrimalEditor.Content
                 case PrimitiveMeshType.Plane:
                     {
                         info.SegmentX = (int)xSliderPlane.Value;
-                        info.SegmentZ = (int)xSliderPlane.Value;
+                        info.SegmentZ = (int)zSliderPlane.Value;
                         info.Size.X = Value(widthScalarBoxPlane, 0.001f);
                         info.Size.Z = Value(lengthScalarBoxPlane, 0.001f);
                         break;
                     }
                 case PrimitiveMeshType.Cube:
-                    break;
+                    return;
                 case PrimitiveMeshType.UvSphere:
-                    break;
+                    return;
                 case PrimitiveMeshType.IcoSphere:
-                    break;
+                    return;
                 case PrimitiveMeshType.Cyclinder:
-                    break;
+                    return;
                 case PrimitiveMeshType.Capsule:
-                    break;
+                    return;
                 default:
-                    throw new ArgumentOutOfRangeException(nameof(primitiveType));
+                    return;/*throw new ArgumentOutOfRangeException(nameof(primitiveType));*/
             }
             var geometry = new Geometry();
             ContentToolsAPI.CreatePrimitiveMesh(geometry, info);
             (DataContext as GeometryEditor).SetAsset(geometry);
+            OnTexture_CheckBox_Click(textureCheckBox, null);
+        }
+
+        private static void LoadTextures()
+        {
+            var uris = new List<Uri>
+            {
+                new Uri("pack://application:,,,/Resources/PrimitiveMeshView/A.png"),
+            };
+            _textures.Clear();
+            foreach (var uri in uris)
+            {
+                var resource = Application.GetResourceStream(uri);
+                using var reader = new BinaryReader(resource.Stream);
+                var data = reader.ReadBytes((int)resource.Stream.Length);
+                var imageSource = (BitmapSource)new ImageSourceConverter().ConvertFrom(data);
+                imageSource.Freeze();
+                var brush = new ImageBrush(imageSource);
+                brush.Transform = new ScaleTransform(1, -1, 0.5, 0.5);
+                brush.ViewportUnits = BrushMappingMode.Absolute;
+                brush.Freeze();
+                _textures.Add(brush);
+            }
+        }
+
+        static PrimitiveMeshDialog()
+        {
+            LoadTextures();
         }
 
         public PrimitiveMeshDialog()
         {
             InitializeComponent();
             Loaded += (sender, args) => UpdatePrimitive();
+        }
+
+        private void OnTexture_CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            Brush brush = Brushes.White;
+            if ((sender as CheckBox).IsChecked == true)
+            {
+                brush = _textures[(int)PrimitiveMeshTypeComboBox.SelectedItem];
+            }
+
+            var vm = DataContext as GeometryEditor;
+
+            foreach (var mesh in vm.MeshRenderer.Meshes)
+            {
+                mesh.Diffuse = brush;
+            }
         }
     }
 }
